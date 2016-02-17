062560b12d7f74eaae680a0c67b86b26144c6909
markngsq <markngshiqiang@gmail.com>
2016-02-17 12:57:09 +0000

    Append function + array formatting
    added in the append function, php won’t overwrite existing arrays but
    add on to the file.
    
    Still have not solved the formatting for the form itself, can’t seem to
    figure out how to add a colon in or remove inverted commas where i
    don’t want it to.

--- dist/save_json.php
+++ dist/save_json.php
@@ -1,31 +1,53 @@
 <?php
-// Saving data from form in text file in JSON format
+// Append new form data in json string saved in text file
 // From: http://coursesweb.net/php-mysql/
 
+// path and name of the file
+$filetxt = 'dirdata/formdata.txt';
+
 // check if all form data are submited, else output error message
-if(isset($_POST['user']) && isset($_POST['splash']) && isset($_POST['qn1']) && isset($_POST['qn2']) && isset($_POST['qn3']) && isset($_POST['qn4']) && isset($_POST['qn5'])) {
-  // if form fields are empty, outputs message, else, gets their data
-  if(empty($_POST['user']) || empty($_POST['splash']) || empty($_POST['qn1']) || empty($_POST['qn2']) || empty($_POST['qn3']) || empty($_POST['qn4']) || empty($_POST['qn5'])) {
+if(isset($_POST['user']) && isset($_POST['splash']) && isset($_POST['qn1']) && isset($_POST['qn2']) && isset($_POST['qn3']) && isset($_POST['qn4']) && isset($_POST['qn5']) && isset($_POST['idd'])) {
+// if form fields are empty, outputs message, else, gets their data
+if(empty($_POST['user']) || empty($_POST['splash']) || empty($_POST['qn1']) || empty($_POST['qn2']) || empty($_POST['qn3']) || empty($_POST['qn4']) || empty($_POST['qn5']) || empty($_POST['idd'])) {
     echo 'All fields are required';
   }
   else {
-    // adds form data into an array
+// gets and adds form data into an array
     $formdata = array(
-      name=> $_POST['user'],
-      splash=> $_POST['splash'],
-      qn1=> $_POST['qn1'],
-      qn2=> $_POST['qn2'],
-      qn3=> $_POST['qn3'],
-      qn4=> $_POST['qn4'],
-      qn5=> $_POST['qn5']
+      $_POST['idd'],
+      array(
+        name=> $_POST['user'],
+        splash=> $_POST['splash'],
+        qn1=> $_POST['qn1'],
+        qn2=> $_POST['qn2'],
+        qn3=> $_POST['qn3'],
+        qn4=> $_POST['qn4'],
+        qn5=> $_POST['qn5'],
+      )
+
     );
+
+// path and name of the file
+    $filetxt = 'dirdata/formdata.txt';
+
+    $arr_data = array();        // to store all form data
+
+// check if the file exists
+    if(file_exists($filetxt)) {
+   // gets json-data from file
+    $jsondata = file_get_contents($filetxt);
+   // converts json string into array
+    $arr_data = json_decode($jsondata, true);
+  }
+// appends the array with new form data
+    $arr_data[] = $formdata;
 
     // encodes the array into a string in JSON format (JSON_PRETTY_PRINT - uses whitespace in json-string, for human readable)
     $jsondata = json_encode($formdata, JSON_PRETTY_PRINT);
 
     // saves the json string in "formdata.txt" (in "dirdata" folder)
     // outputs error message if data cannot be saved
-    if(file_put_contents('dirdata/formdata.txt', $jsondata)) echo 'Data successfully saved';
+    if(file_put_contents('dirdata/formdata.txt', $jsondata, FILE_APPEND)) echo 'Data successfully saved';
     else echo 'Unable to save data in "dirdata/formdata.txt"';
   }
 }
